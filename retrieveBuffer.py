617347ad404b9369de1dcb4b6c809ffadd51f15b
AryehSchoefer <aryeh.shmuel@googlemail.com>
2021-01-28 11:21:09 +0100

    added updateBufferValue method to enable on-the-fly updating in the future

--- retrieveBuffer.py
+++ retrieveBuffer.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv, find_dotenv
 
 class Buffer:
     def __init__(self, databasename = 'rlf-shadowplay-cluster', collectionname = 'buffers'):
+        self.databasename = databasename
+        self.collectionname = collectionname
+        
         # Check if config.json does exist
         if os.path.isfile('config.json'):
             # load config.json and get buffer value
@@ -12,30 +15,33 @@ class Buffer:
                 data = json.load(fin)
                 self.__bufferValue = data['buffer']['value']
         else:
-            # load .env file
-            load_dotenv(find_dotenv())
-            MONGO_URI = os.environ.get('MONGO_URI')
+            self.updateBufferValue() # connect to database and retrieve buffer value
+    
+    def updateBufferValue(self):
+        # load .env file
+        load_dotenv(find_dotenv())
+        MONGO_URI = os.environ.get('MONGO_URI')
 
-            # connect to database
-            client = pymongo.MongoClient(MONGO_URI)
-            database = client[databasename]
-            collection = database[collectionname]
+        # connect to database
+        client = pymongo.MongoClient(MONGO_URI)
+        database = client[self.databasename]
+        collection = database[self.collectionname]
+        
+        # get database data and get buffer value
+        data = collection.find({})
+        for item in data:
+            if item['date']: # check if data came from website (needs to be reworked)
+                self.__bufferValue = item['buffer']
+                break
             
-            # get database data and get buffer value
-            data = collection.find({})
-            for item in data:
-                if item['date']: # check if data came from website (needs to be reworked)
-                    self.__bufferValue = item['buffer']
-                    break
-                
-            # write buffer value to config.json (simultaneously creating config.json)
-            with open('config.json', 'w') as fout:
-                config = {}
-                config['buffer'] = { 'value': self.__bufferValue }
-                json.dump(config, fout)
+        # write buffer value to config.json (simultaneously creating config.json)
+        with open('config.json', 'w') as fout:
+            config = {}
+            config['buffer'] = { 'value': self.__bufferValue }
+            json.dump(config, fout)
             
     def getBufferValue(self):
         return self.__bufferValue
-            
+    
 if __name__ == '__main__':
     print(Buffer().getBufferValue())
