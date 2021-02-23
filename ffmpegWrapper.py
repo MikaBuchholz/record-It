896fb075d5c0c9e517ee764da31016e49c265500
MikaBuchholz <mika.buchholz3@gmail.com>
2021-02-23 16:08:45 +0100

    Changed imports

--- ffmpegWrapper.py
+++ ffmpegWrapper.py
@@ -1,40 +1,39 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-import ffmpeg
 from os import remove, system
-import cv2
+from cv2 import VideoCapture, CAP_PROP_FPS, CAP_PROP_FRAME_COUNT
 
 class FfmpegWrapper:
     def __init__(self):
-        pass
+        self.videoType = 'h264'
 
     def concatVideos(self, firstVideo, secondVideo, filename = 'output'):
         self.writeVideosToTxt(firstVideo, secondVideo)
 
-        system(f'ffmpeg -f concat -i mylist.txt -c copy {filename}.mp4')
+        system(f'ffmpeg -f concat -i mylist.txt -c copy {filename}.{self.videoType}')
         remove('mylist.txt')
 
         return True
     
     def concatVideoAndAudio(self, video, audio, outputName = 'output'):
-        system(f'ffmpeg -i {video}.mp4 -i {audio}.mp3 -c:v copy -c:a aac {outputName}.mp4')
+        system(f'ffmpeg -i {video}.{self.videoType} -i {audio}.mp3 -c:v copy -c:a aac {outputName}.{self.videoType}')
 
         return True
         
     def writeVideosToTxt(self, firstVideo, secondVideo):
         with open('mylist.txt', 'w') as file:
-            file.write(f'file {firstVideo}.mp4\nfile {secondVideo}.mp4')
+            file.write(f'file {firstVideo}.{self.videoType}\nfile {secondVideo}.{self.videoType}')
             
         return True
 
     def extractVideoClip(self, file, startTime, endTime, filename = 'subclip'): 
-         ffmpeg_extract_subclip(f'{file}.mp4', startTime, endTime, targetname= f'{filename}.mp4')
+         ffmpeg_extract_subclip(f'{file}.{self.videoType}', startTime, endTime, targetname= f'{filename}.{self.videoType}')
 
          return True
 
     def getVideoLength(self, file):
-        video = cv2.VideoCapture(f'./{file}.mp4')
-        fps = video.get(cv2.CAP_PROP_FPS)
-        frameCount = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        video = VideoCapture(f'./{file}.{self.videoType}')
+        fps = video.get(CAP_PROP_FPS)
+        frameCount = video.get(CAP_PROP_FRAME_COUNT)
         length = int(frameCount / fps)
         
         return length #Sekunden
