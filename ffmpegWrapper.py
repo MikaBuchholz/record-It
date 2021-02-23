1ba0748c9602dd0fd6d42b6f44bea29772c501be
AryehSchoefer <aryeh.shmuel@googlemail.com>
2021-02-23 16:13:12 +0100

    changed to static class

--- ffmpegWrapper.py
+++ ffmpegWrapper.py
@@ -1,37 +1,38 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-import ffmpeg
 from os import remove, system
-import cv2
+from cv2 import VideoCapture, CAP_PROP_FPS, CAP_PROP_FRAME_COUNT
 
 class FfmpegWrapper:
-    def __init__(self):
-        pass
-
-    def concatVideos(self, firstVideo, secondVideo, filename = 'output'):
-        self.writeVideosToTxt(firstVideo, secondVideo)
+    @staticmethod
+    def concatVideos(firstVideo, secondVideo, filename = 'output'):
+        FfmpegWrapper.writeVideosToTxt(firstVideo, secondVideo)
 
         system(f'ffmpeg -f concat -i mylist.txt -c copy {filename}.mp4')
         remove('mylist.txt')
 
         return True
     
-    def concatVideoAndAudio(self, video, audio, outputName = 'output'):
+    @staticmethod
+    def concatVideoAndAudio(video, audio, outputName = 'output'):
         system(f'ffmpeg -i {video}.mp4 -i {audio}.mp3 -c:v copy -c:a aac {outputName}.mp4')
 
         return True
-        
-    def writeVideosToTxt(self, firstVideo, secondVideo):
+    
+    @staticmethod
+    def writeVideosToTxt(firstVideo, secondVideo):
         with open('mylist.txt', 'w') as file:
             file.write(f'file {firstVideo}.mp4\nfile {secondVideo}.mp4')
             
         return True
 
-    def extractVideoClip(self, file, startTime, endTime, filename = 'subclip'): 
+    @staticmethod
+    def extractVideoClip(file, startTime, endTime, filename = 'subclip'): 
          ffmpeg_extract_subclip(f'{file}.mp4', startTime, endTime, targetname= f'{filename}.mp4')
 
          return True
 
-    def getVideoLength(self, file):
+    @staticmethod
+    def getVideoLength(file):
         video = cv2.VideoCapture(f'./{file}.mp4')
         fps = video.get(cv2.CAP_PROP_FPS)
         frameCount = video.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -41,4 +42,4 @@ class FfmpegWrapper:
     
 if __name__ == '__main__':
     #FfmpegWrapper().concatVideos('TestFile157', 'TestFile278')
-    print(FfmpegWrapper().getVideoLength('SexAv'))
+    print(FfmpegWrapper().getVideoLength('Test'))
