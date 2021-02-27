e6cae4db2f867bbd65c5fda85ef90d0fe19a4fff
MikaBuchholz <mika.buchholz3@gmail.com>
2021-02-27 18:40:55 +0100

    added button press value getter and function to change its value in config.json

--- retrieveBuffer.py
+++ retrieveBuffer.py
@@ -4,9 +4,10 @@ import pymongo
 from dotenv import load_dotenv, find_dotenv
 
 class Buffer:
-    def __init__(self, databasename = 'rlf-shadowplay-cluster', collectionname = 'buffers'):
+    def __init__(self, databasename = 'rlf-shadowplay-cluster', collectionname = 'buffers', secondcollectionname = 'captures'):
         self.databasename = databasename
         self.collectionname = collectionname
+        self.secondcollectionname = secondcollectionname
         
         # Check if config.json does exist
         if os.path.isfile('config.json'):
@@ -26,17 +27,23 @@ class Buffer:
         # connect to database
         client = pymongo.MongoClient(MONGO_URI)
         database = client[self.databasename]
-        collection = database[self.collectionname]
+        self.collection = database[self.collectionname]
+        self.secondCollection = database[self.secondcollectionname]
         
         # get database data and get buffer value
-        data = collection.find({})
-        for item in data:
+        primaryData = self.collection.find({})
+        for item in primaryData:
             if item['date']: # check if data came from website (needs/can to be reworked)
                 self.__bufferValue = item['buffer']
                 self.__recording = item['recording']
+                break
+        
+        secondaryData = self.secondCollection.find({})
+        for item in secondaryData:
+            if item['date']:
                 self.__btnPress = item['btnPressed']
                 break
-            
+
         # write buffer value to config.json (simultaneously creating config.json)
         with open('config.json', 'w') as fout:
             config = {}
@@ -51,6 +58,22 @@ class Buffer:
     def getRecordingValue(self):
         return self.__recording
     
+    def getBtnPress(self):  
+        for item in self.secondCollection.find({}):
+            if item['model']:
+                return item['btnPressed']
+    
+    def modifyPressedValue(self, configInput = False):
+        buffer = self.getBufferValue()
+        recordVal = self.getRecordingValue()
+
+        with open('config.json', 'w') as fout:
+            config = {}
+            config['buffer'] = { 'value': buffer}
+            config['recording'] = recordVal
+            config['btnPressed'] = configInput
+            json.dump(config, fout)
+
 if __name__ == '__main__':
     Buffer().updateConfig()
     print(Buffer().getBufferValue())
