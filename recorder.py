faf0378d1cbf4b76cd8fd4bde8004cc6cbbb1262
AryehSchoefer <aryeh.shmuel@googlemail.com>
2021-03-02 16:28:59 +0100

    Added button press functionality in recorder.py

--- recorder.py
+++ recorder.py
@@ -21,15 +21,15 @@ class Recorder():
         if not path.isdir(f'{curdir}\\finishedClips'):
             mkdir(f'{curdir}\\finishedClips')
 
-    def startRecording(self):
+    def startRecording(self, session):
         outputName = (datetime.now()).strftime('%d%m%Y%H%M%S')
-        self.camera.start_recording(f'{outputName}.h264') # PiCamera library recording
+        self.camera.start_recording(f'{outputName}.h264') # PiCamera library recording 
         endTime = time() + (2 * self.buffer)
         
         while True:
             currentTime = time()
             
-            if currentTime >= endTime: # Muss noch zum Knopfdruck geändert werden
+            if session.getBtnPress():
                 self.camera.stop_recording()
                 self.checkVideoVsBufferLength()
                 break
@@ -66,7 +66,7 @@ class Recorder():
                 remove('bufferSubClip.h264')
                 move(f'{curdir}//{outputName}', f'{curdir}\\finishedClips')
         
-        if curVideoLength > self.buffer: # or ManageVideos().checkRawFolderEmpty(self):
+        if curVideoLength > self.buffer:
             startTime = curVideoLength - self.buffer
             endTime = curVideoLength
             
@@ -77,14 +77,15 @@ class Recorder():
     def convertToMp4(self, fileName, outputName):
             system(f'MP4Box -add {fileName}.h264 {outputName}.mp4')
             remove(f'{fileName}.h264')
-            print('Done')
             
 if __name__ == '__main__':
     session = Buffer()
+    session.toggleBtnPress(False)
     
     while session.getRecordingValue():
-        Recorder().startRecording()
-        session.updateConfig()
+        Recorder().startRecording(session)
+        session.updateValues()
+        session.toggleBtnPress(False) # maybe time.sleep davor
 
 '''
             [/////]
